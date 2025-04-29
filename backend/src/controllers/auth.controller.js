86e8124cfe3557ae6c137851deb7eecf4b1488a3
Qanta-31 <arnavkant31@gmail.com>
2025-04-29 22:38:08 +0530

    auth.controller.js changed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -105,7 +105,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.cookie("jwt", "", { maxAge: 0 });
+        res.cookie("jwt", "", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "None",
+            expires: new Date(0),
+        });
         res.status(200).json({ message: 'Logged out successfully' });
     } catch (error) {
         console.log("Error in logout controller", error.message);
