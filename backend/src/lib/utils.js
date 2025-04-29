2891a23a34bd38b652ceb006fcc59e9746fd6b34
Qanta-31 <arnavkant31@gmail.com>
2025-04-29 22:01:55 +0530

    utils.js changed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,6 +9,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days
         httpOnly: true,  // Makes the cookie accessible only by the server
         secure: process.env.NODE_ENV !== "development",  // In development, cookies are not sent over https
+        sameSite:"None",
     });
 
     return token;
