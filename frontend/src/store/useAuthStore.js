ce74a3abef8d2c3dd59107d1be82f40efb64871b
Qanta-31 <arnavkant31@gmail.com>
2025-04-29 14:54:23 +0530

    Changed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,6 +3,7 @@ import { axiosInstance } from '../lib/axios.js'
 import toast from 'react-hot-toast';
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+console.log("BASE_URL being used:", BASE_URL);
 
 export const useAuthStore = create((set) => ({
     authUser: null,
