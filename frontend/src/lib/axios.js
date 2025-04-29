5d38f8a424b72328372f3679c73eb9cf92ac2cef
Qanta-31 <arnavkant31@gmail.com>
2025-04-29 19:23:46 +0530

    asdf

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://fitness-tracking-app-backend-i588.onrender.com/api", 
+    baseURL: "https://fitness-tracking-app-backend-i588.onrender.com/api", 
     withCredentials: true,
 })
